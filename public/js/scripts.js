172dfe566bb387c8b5a6d4608e349e3446f36818
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2024-03-13 23:30:12 +0100

    added ui modal and changed icon to image

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,28 +1,57 @@
 // Not Permitted Modal - Project Page
-const openNotPermittedModal = document.querySelectorAll("#openNotPermittedModal");
-const CloseNotPermittedModal = document.querySelector("#closeNotPermittedModal");
+const openNotPermittedModal = document.querySelectorAll(
+  "#openNotPermittedModal"
+);
+const CloseNotPermittedModal = document.querySelector(
+  "#closeNotPermittedModal"
+);
 const notPermittedModal = document.querySelector("#notPermittedModal");
 
-notPermittedModal.addEventListener("click", e => {
-  const notPermittedModalDimensions = notPermittedModal.getBoundingClientRect()
+notPermittedModal.addEventListener("click", (e) => {
+  const notPermittedModalDimensions = notPermittedModal.getBoundingClientRect();
   if (
     e.clientX < notPermittedModalDimensions.left ||
     e.clientX > notPermittedModalDimensions.right ||
     e.clientY < notPermittedModalDimensions.top ||
     e.clientY > notPermittedModalDimensions.bottom
   ) {
-    notPermittedModal.close()
+    notPermittedModal.close();
   }
 });
 
-//Because id is use multiple times across the project page
-openNotPermittedModal.forEach(link => {
-  link.addEventListener('click', () => {
+//Because id is used multiple times across the project page
+openNotPermittedModal.forEach((link) => {
+  link.addEventListener("click", () => {
     notPermittedModal.showModal();
   });
 });
 
-CloseNotPermittedModal.addEventListener('click', () => {
+CloseNotPermittedModal.addEventListener("click", () => {
   notPermittedModal.close();
 });
 
+// Not Logged In Modal - Project Page
+const openNotLoggedInModal = document.querySelector("#openNotLoggedInModal");
+const closeNotLoggedInModal = document.querySelector("#closeNotLoggedInModal");
+const notLoggedInModal = document.querySelector("#notLoggedInModal");
+notLoggedInModal.showModal();
+// notLoggedInModal.addEventListener("click", e => {
+//   const notLoggedInModalDimensions = notLoggedInModal.getBoundingClientRect()
+//   if (
+//     e.clientX < notLoggedInModalDimensions.left ||
+//     e.clientX > notLoggedInModalDimensions.right ||
+//     e.clientY < notLoggedInModalDimensions.top ||
+//     e.clientY > notLoggedInModalDimensions.bottom
+//   ) {
+//     notLoggedInModal.close()
+//   }
+// });
+
+//Because id is used multiple times across the project page
+openNotLoggedInModal.addEventListener("click", () => {
+  notLoggedInModal.showModal();
+});
+
+closeNotLoggedInModal.addEventListener("click", () => {
+  notLoggedInModal.close();
+});

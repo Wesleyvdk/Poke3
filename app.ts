a3bf3488fd57ec5436bf4018fb0058d8622a97cd
Wesleyvdk <shinxisrl@outlook.com>
2024-04-24 07:39:03 +0000

    HTML to EJS and express

---
+++ app.ts
@@ -0,0 +1,56 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    return res.render("index");
+});
+
+app.get("/capture", async (req, res) => {
+    return res.render("capture");
+})
+app.get("/compare", async (req, res) => {
+    return res.render("compare");
+})
+
+app.get("/pokedex", async (req, res) => {
+    return res.render("pokedex")
+})
+
+app.get("/landing", async (req, res) => {
+    return res.render("landing")
+})
+
+app.get("/login", async (req, res) => {
+    return res.render("login")
+});
+
+app.get("/register", async (req, res) => {
+    return res.render("register")
+})
+
+app.get("/battle", async (req, res) => {
+    return res.render("pokemonBattle")
+})
+
+app.get("/quiz", async (req, res) => {
+    return res.render("quiz")
+})
+
+app.get("/starter", async (req, res) => {
+    return res.render("starter")
+})
+
+app.listen(3000, async () => {
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon");
+    // Gebruik het juiste type voor data
+    let data: any = await response.json();
+    console.log(data.results);
+    console.log(`The application is listening on http://localhost:3000`);
+})
